5a08a2c9c06b6805ea1745df6a73f69a342a2a06
Alex Lance <alexander.lance@gmail.com>
2021-09-27 13:35:52 -0600

    Added Events in Practice example code

---
+++ 2-how-node-works/starter/events.js
@@ -0,0 +1,47 @@
+const EventEmitter = require('events');
+const http = require('http');
+
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
+
+// Observer 1
+myEmitter.on('newSale', () => {
+  console.log('There was a new sale!');
+});
+
+// Observer 2
+myEmitter.on('newSale', () => {
+  console.log('Customer name: Jonas');
+});
+
+// Observer 3
+myEmitter.on('newSale', (stock) => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+
+myEmitter.emit('newSale', 9);
+
+//******************************************************* */
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log('Request received.');
+  res.end('Request received');
+});
+
+server.on('request', (req, res) => {
+  console.log('Another request');
+});
+
+server.on('close', (req, res) => {
+  res.end('Server closed');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Waiting for requrestes...');
+});
