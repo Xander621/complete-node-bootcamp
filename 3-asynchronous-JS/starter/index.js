520cbde8e19cf0bf1162477abbb7c69d9d196fff
Alex Lance <alex@lance-technologies.com>
2021-09-27 18:47:18 -0600

    Added 'From Callback Hell to Promises' exercise

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -5,13 +5,17 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   if (err) return console.log(err.message);
   console.log(`Breed: ${data}`);
 
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-    if (err) return console.log(err.message);
-    console.log(res.body.message);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      console.log(res.body.message);
 
-    fs.writeFile('dog-image.txt', res.body.message, (err) => {
-      if (err) return console.log(err.message);
-      console.log('Random dog image saved to file');
+      fs.writeFile('dog-image.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('Random dog image saved to file');
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
-  });
 });
