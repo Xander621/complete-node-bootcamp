0fbafe830519b5df75a64f78ba96be3b8e28d796
Alex Lance <alex@lance-technologies.com>
2021-09-27 19:36:33 -0600

    Added 'Consuming Promises w/ Async/Await' exercise

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,20 +19,41 @@ const writeFilePromise = (file, data) => {
   });
 };
 
-readFilePromise(`${__dirname}/dog.txt`)
-  .then((data) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePromise(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
+    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
     console.log(res.body.message);
 
-    return writeFilePromise('dog-image.txt', res.body.message);
-  })
-  .then(() => {
+    await writeFilePromise('dog-image.txt', res.body.message);
     console.log('Random dog image saved to file');
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDogPic();
+
+/**
+ *  The following code shows how to write, chain and consume promises.
+ *  It is a bit verbose compared to consuming promises with async/await above.
+ */
+// readFilePromise(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+
+//     return writeFilePromise('dog-image.txt', res.body.message);
+//   })
+//   .then(() => {
+//     console.log('Random dog image saved to file');
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
